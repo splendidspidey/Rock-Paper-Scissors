d46806ec7e08585781c08b6122e5190e5301f4d9
SplendidSpidey <95857725+splendidspidey@users.noreply.github.com>
2021-12-18 10:33:05 +0000

    Changed the 'ai' variable to 'computer'

--- rpstest.py
+++ rpstest.py
@@ -1,27 +1,27 @@
 from random import randint
 choice = ["Rock", "Paper", "Scissors"]
-ai = choice[randint(0,2)]
+computer = choice[randint(0,2)]
 player = False
 while player == False:
     player = input("Rock, Paper or Scissors? ")
-    if player == ai:
+    if player == computer:
         print("Haha, You played the same move!")
     elif player == "Rock":
-        if ai == "Paper":
-            print("Welp, You lost!", ai, "covers", player)
+        if computer == "Paper":
+            print("Welp, You lost!", computer, "covers", player)
         else:
-            print("Woohoo, You win!", player, "smashes", ai)
+            print("Woohoo, You win!", player, "smashes", computer)
     elif player == "Paper":
-        if ai == "Scissors":
-            print("Welp, You lose!", ai, "cut", player)
+        if computer == "Scissors":
+            print("Welp, You lose!", computer, "cut", player)
         else:
-            print("AYYY, You win!", player, "covers", ai)
+            print("AYYY, You win!", player, "covers", computer)
     elif player == "Scissors":
-        if ai == "Rock":
-            print("You lose...", ai, "smashes", player)
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
         else:
-            print("Nice one, You win!", player, "cut", ai)
+            print("Nice one, You win!", player, "cut", computer)
     else:
         print("Are you sure you entered that correctly...")
     player = False
-    ai = choice[randint(0,2)]
+    computer = choice[randint(0,2)]
